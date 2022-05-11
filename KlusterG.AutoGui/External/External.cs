e40ccaeacd0a663eaffebaa73108139d0f0aeead
Guilherme Marques <guianselmi.sa@hotmail.com>
2022-05-11 13:08:38 -0300

    Adicionar arquivos de projeto.

---
+++ KlusterG.AutoGui/External/External.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace KlusterG.AutoGui
+{
+    public class External
+    {
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        [DllImport("user32.dll", EntryPoint = "MessageBox")]
+        public static extern int message(int hWnd, String text, String caption, uint type);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out Structs.CursorPosition lpPoint);
+
+        [DllImport("user32.dll")]
+        public static extern void SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        public static extern Int16 GetAsyncKeyState(int vKey);
+
+        [DllImport("user32.dll")]
+        public static extern bool SendInput(uint xInputs, Structs.INPUT[] pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetKeyState(int nVirtKey);
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+    }
+}
---
+++ KlusterG.AutoGui/InternalKeys/MKeys.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlusterG.AutoGui
+{
+    public enum MKeys
+    {
+        [EnumMember]
+        LeftDrop = 0x00000002,
+
+        [EnumMember]
+        LeftPress = 0x00000004,
+
+        [EnumMember]
+        RightDrop = 0x00000008,
+
+        [EnumMember]
+        RightPress = 0x00000010,
+
+        [EnumMember]
+        MiddleDrop = 0x00000020,
+
+        [EnumMember]
+        MiddlePress = 0x00000040,
+
+        [EnumMember]
+        Move = 0x00000001,
+
+        [EnumMember]
+        Absolute = 0x00008000,
+    }
+}
